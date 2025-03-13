ba694d637ef0f7ae912b98a09983dc16acce4034
Edmarck Sosa Pineda <143844680+Lapseyy@users.noreply.github.com>
2025-03-12 17:37:19 -0700

    Clean main file.

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from games import query_player, alpha_beta_player
 
 if __name__ == '__main__':
     print("Welcome to the Game of Nim!")
-    initial_board = [7, 5, 3, 1]  # You can modify this to test different configurations
+    initial_board = [7, 5, 3, 1]  
 
-    # Initialize and play
+    # Initialize and play human vs. AI
     game = GameOfNim(initial_board)
-    result = game.play_game(query_player, alpha_beta_player)  # Human vs. AI
+    result = game.play_game(query_player, alpha_beta_player) 
 
-    # Display result
     if result == 1:
         print("MAX won the game")
     elif result == -1:
